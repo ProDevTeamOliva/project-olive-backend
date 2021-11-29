6f5aa8ddc7558592ced2a47ca800390d86e9c17d
pauljackals <pauljackals@users.noreply.github.com>
2021-11-29 17:43:57 +0000

    Prettified Code!

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const mongoStore = require("./config/mongoStore");
 
 const server = http.createServer(app);
 
-app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
