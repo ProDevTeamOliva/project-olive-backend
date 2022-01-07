fdaedf2feb36e3540d25993b8c8a103e08ee9b34
Pawel Sakaluk <paul.jackals@gmail.com>
2022-01-07 16:01:08 +0100

    PO-80_friends_adding_accepting_and_deleting

--- routes/me.js
+++ routes/me.js
@@ -34,10 +34,48 @@ router.get("/", async (req, res) => {
 router.get("/friend", async (req, res) => {
   const id = req.user._id;
 
-  return res.status(200).json({
-    users: [],
-    message: "apiMyFriendsSuccess",
-  });
+  const result = {
+    friends: [],
+    pendingSent: [],
+    pendingReceived: []
+  }
+
+  const session = neo4jDriver.session();
+  session
+    .run("MATCH (u1:User {sessionUserID: $sessionUserID})-[r:PENDING|FRIEND]-(u2:User) RETURN u2,r", {
+      sessionUserID: id.toString(),
+    })
+    .subscribe({
+      onNext: (record) => {
+        const {u2, r} = record.toObject()
+        const user = u2.properties
+        user.sessionUserID = undefined
+
+        if(r.type==="FRIEND") {
+          result.friends.push(user)
+
+        } else if (r.type==="PENDING") {
+          const u2Identity = u2.identity
+          if(r.start===u2Identity) {
+            result.pendingReceived.push(user)
+
+          } else if (r.end===u2Identity) {
+            result.pendingSent.push(user)
+          }
+        }
+      },
+      onCompleted: () => {
+        session.close();
+        return res.status(200).json({
+          ...result,
+          message: "apiMyFriendsSuccess",
+        });
+      },
+      onError: (error) => {
+        session.close();
+        return res.status(500).json({ message: "apiServerError" });
+      },
+    });
 });
 
 router.get("/post", async (req, res) => {
--- routes/user.js
+++ routes/user.js
@@ -44,6 +44,105 @@ router.get("/:id/post", async (req, res) => {
   });
 });
 
+router.post("/:id/friend", async (req, res) => {
+  const idSource = req.user._id
+  const idTarget = req.params.id
+  let result = false
+
+  const session = neo4jDriver.session();
+  session
+    .run("MATCH (u1:User{sessionUserID: $sessionUserID}) MATCH (u2:User{id: $id}) WHERE NOT exists((u1)-[:PENDING|FRIEND]-(u2)) MERGE (u1)-[p:PENDING]-(u2) RETURN u1,p,u2", {
+      sessionUserID: idSource.toString(),
+      id: idTarget
+    })
+    .subscribe({
+      onNext: (record) => {
+        result = true
+
+      },
+      onCompleted: () => {
+        session.close();
+        if (result) {
+          return res.status(201).json({
+            message: "apiFriendPendingSuccess",
+          });
+        } else {
+          return res.status(400).json({ message: "apiFriendPendingError" });
+        }
+      },
+      onError: (error) => {
+        session.close();
+        return res.status(500).json({ message: "apiServerError" });
+      },
+    });
+})
+
+router.post("/:id/accept", async (req, res) => {
+  const idSource = req.user._id
+  const idTarget = req.params.id
+  let result = false
+
+  const session = neo4jDriver.session();
+  session
+    .run("MATCH (u1:User{sessionUserID: $sessionUserID})<-[p:PENDING]-(u2:User{id: $id}) DELETE p MERGE (u1)-[f:FRIEND]-(u2) RETURN u1,f,u2", {
+      sessionUserID: idSource.toString(),
+      id: idTarget
+    })
+    .subscribe({
+      onNext: (record) => {
+        result = true
+
+      },
+      onCompleted: () => {
+        session.close();
+        if (result) {
+          return res.status(201).json({
+            message: "apiFriendAcceptSuccess",
+          });
+        } else {
+          return res.status(400).json({ message: "apiFriendAcceptError" });
+        }
+      },
+      onError: (error) => {
+        session.close();
+        return res.status(500).json({ message: "apiServerError" });
+      },
+    });
+})
+
+router.delete("/:id/friend", async (req, res) => {
+  const idSource = req.user._id
+  const idTarget = req.params.id
+  let result = false
+
+  const session = neo4jDriver.session();
+  session
+    .run("MATCH (u1:User{sessionUserID: $sessionUserID})-[r:PENDING|FRIEND]-(u2:User{id: $id}) DELETE r RETURN u1,u2", {
+      sessionUserID: idSource.toString(),
+      id: idTarget
+    })
+    .subscribe({
+      onNext: (record) => {
+        result = true
+
+      },
+      onCompleted: () => {
+        session.close();
+        if (result) {
+          return res.status(200).json({
+            message: "apiFriendRemoveSuccess",
+          });
+        } else {
+          return res.status(400).json({ message: "apiFriendRemoveError" });
+        }
+      },
+      onError: (error) => {
+        session.close();
+        return res.status(500).json({ message: "apiServerError" });
+      },
+    });
+})
+
 router.get("/:id/picture", async (req, res) => {
   const id = req.params.id;
 
