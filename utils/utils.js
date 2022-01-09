890c74175fa936a64c3a5540b9bfcb3f5c518c52
Marek Połom <marekpolom22@gmail.com>
2022-01-09 03:51:51 +0100

    PO-45_/me/pictures_POST_endpoint

---
+++ utils/utils.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+function saveBase64Picture(filepath, image) {
+    image = image.split('base64,')[1];
+
+    fs.writeFile(filepath, image, { encoding: 'base64' }, function (err) {
+        if(err){
+            console.log(err);
+        }
+    });
+}
+
+module.exports = { saveBase64Picture };
