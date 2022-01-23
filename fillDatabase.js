01199b3f598829626b45846382518e99925ee33f
Marek Połom <marekpolom22@gmail.com>
2022-01-23 20:07:52 +0100

    PO-94_fixed db fill script for user avatar

--- fillDatabase.js
+++ fillDatabase.js
@@ -161,7 +161,7 @@ const main = async () => {
         nameLast,
         login: userSaved.login,
         sessionUserID: userSaved._id.toString(),
-        avatar: "/public/pictures/avatar_default.jpg",
+        avatar: "/public/pictures/avatar_default.png",
       }
     );
     await session.close();
