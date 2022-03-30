68dd2d0cd8510d54f1460a2cf08fb134f30eff76
Marek Połom <marekpolom22@gmail.com>
2022-03-30 23:21:37 +0200

    PO-120_moved some configuration from index.js to seperate files

---
+++ config/originHost.js
@@ -0,0 +1,3 @@
+const originHost = (parseInt(process.env.HTTPS)) ? `https://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}` : `http://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}`;
+
+module.exports = originHost;
