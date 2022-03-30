602b7739f1e6ca2e1fb56c8033fdc0866cc597a6
Marek Połom <marekpolom22@gmail.com>
2022-03-30 23:22:01 +0200

    PO-120_web socket configuration and endpoints

---
+++ socket/socket.js
@@ -0,0 +1,21 @@
+const sio = require("../config/socket");
+const AuthenticationError = require("passport/lib/errors/authenticationerror");
+const { authenticationCheck, wrapMiddleware } = require("../utils/middlewares");
+
+const authenticationCheckWrapped = wrapMiddleware(authenticationCheck);
+
+const getId = socket => socket.nsp.name.split("/")[2];
+
+// SAMPLE ENDPOINT
+// ----
+// sio.of(/^\/user\/$/).use(authenticationCheckWrapped).use((socket, next) => {
+//     if (socket.request.user._id.toString() !== getId(socket)) {
+//         return next(new AuthenticationError("apiUnauthorizedError"))
+//     }
+//     next()
+// });
+// ----
+
+console.log("WebSocket initialized!");
+
+module.exports = sio;
