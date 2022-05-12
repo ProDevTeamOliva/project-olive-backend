a95721de14aec2682a7ab9a82456e374ad575903
Marek Połom <marekpolom22@gmail.com>
2022-05-12 19:18:10 +0200

    picture size validation fix

--- utils/validators.js
+++ utils/validators.js
@@ -20,10 +20,11 @@ const validatePicturesSize = (next, picturesArray) => {
     if (!result) return false;
 
     const buffer = Buffer.from(
-      picture.picture.substring(picture.picture.indexOf(",") + 1)
+      picture.picture.substring(picture.picture.indexOf(",") + 1),
+      "base64"
     );
 
-    if (buffer.length <= 3 * 1024 * 1024) {
+    if (buffer.length / 1e+6 <= 3) {
       return true;
     }
 
