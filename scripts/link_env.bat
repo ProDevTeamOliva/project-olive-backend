b7e9df2c566896d3f9ac5a0265cb0397f4e9c4f5
pauljackals <paul.jackals@gmail.com>
2022-05-26 16:17:31 +0200

    PO-183_cleaned_up_scripts

--- scripts/link_env.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SET env_file=.env
-IF EXIST %env_file% DEL %env_file%
-
-powershell.exe start powershell.exe -ArgumentList '-command "Set-Location %cd%; New-Item -ItemType SymbolicLink -Path %env_file% -Target ..\project-olive-main\%env_file%"' -Verb runAs
