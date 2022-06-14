d557dcb26bfadbfce42b87542bf1c9ff1087d9e4
Marek Połom <marekpolom22@gmail.com>
2022-06-14 18:38:54 +0200

    PO-0_auto_test_runner

--- test/checkConnection.js
+++ test/checkConnection.js
@@ -1,24 +1,36 @@
-const neo4j = require("neo4j-driver");
-const { check } = require("prettier");
+if (process.env.RUN_TEST == 2) {
+  const neo4j = require("neo4j-driver");
+  const { config } = require("dotenv");
 
-const username = process.env.NEO4J_USERNAME;
-const password = process.env.NEO4J_PASSWORD;
-const host = process.env.NEO4J_HOST || "localhost";
-const port = process.env.NEO4J_PORT_BOLT_TEST || 7688;
+  config();
 
-const driver = neo4j.driver(
-  `bolt://${host}:${port}`,
-  neo4j.auth.basic(username, password),
-  {
-    disableLosslessIntegers: true,
-  }
-);
+  const username = process.env.NEO4J_USERNAME;
+  const password = process.env.NEO4J_PASSWORD;
+  const host = process.env.NEO4J_HOST || "localhost";
+  const port =
+    process.env.RUN_TEST == 2
+      ? process.env.NEO4J_PORT_BOLT_TEST || 7688
+      : process.env.NEO4J_PORT_BOLT || 7687;
 
-const checkConnection = async () => {
-  let connected = 0;
-  while (!connected) {
-    console.log(await driver.verifyConnectivity());
-  }
-};
+  const driver = neo4j.driver(
+    `bolt://${host}:${port}`,
+    neo4j.auth.basic(username, password),
+    {
+      disableLosslessIntegers: true,
+    }
+  );
 
-module.exports = checkConnection;
+  const verify = () =>
+    driver
+      .verifyConnectivity()
+      .then(() => {
+        console.log("Connected!");
+        process.exit();
+      })
+      .catch((err) => {
+        console.log("Awaiting connection!");
+        setTimeout(verify, 5000);
+      });
+
+  verify();
+}
