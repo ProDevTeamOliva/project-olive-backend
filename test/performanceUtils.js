225fa364233173a5add073d7f5cb87701673071f
pauljackals <pauljackals@users.noreply.github.com>
2022-06-23 14:05:30 +0000

    Prettified Code!

--- test/performanceUtils.js
+++ test/performanceUtils.js
@@ -1,10 +1,10 @@
-const {v4} = require("uuid")
+const { v4 } = require("uuid");
 
 const generateLogin = (context, ee, next) => {
-    context.vars.login = v4().replace(/-/g, "").slice(0, 20)
-    return next()
-}
+  context.vars.login = v4().replace(/-/g, "").slice(0, 20);
+  return next();
+};
 
 module.exports = {
-    generateLogin
-}
+  generateLogin,
+};
