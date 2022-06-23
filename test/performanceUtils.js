de3a5abe58271c301f96c16b8973abdbd90ddfc8
pauljackals <paul.jackals@gmail.com>
2022-06-23 13:04:08 +0200

    PO-189_cleaned_up

--- test/performanceUtils.js
+++ test/performanceUtils.js
@@ -1,25 +1,10 @@
 const {v4} = require("uuid")
 
-// var cookies = []
-
 const generateLogin = (context, ee, next) => {
     context.vars.login = v4().replace(/-/g, "").slice(0, 20)
     return next()
 }
 
-// const afterResponseHandler = (requestParams, response, context, ee, next) => {
-//     context.vars.cookie = response.headers["set-cookie"]
-//     // console.log(context.vars);
-//     return next()
-// }
-
-// const getRandomCookie = (requestParams, context, ee, next) => {
-//     console.log(context.vars);
-//     return next()
-// }
-
 module.exports = {
-    generateLogin,
-    // afterResponseHandler,
-    // getRandomCookie
+    generateLogin
 }
